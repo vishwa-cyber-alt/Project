a1e781bc7638cb1b79d85f9c4bf432b7340dd24a
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-03-08 21:39:46 +0530

    Add files via upload

---
+++ Get Data From SqlServer/Models/Model.cs
@@ -0,0 +1,17 @@
+﻿namespace JobApp.Models
+{
+    public class Model
+    {
+        public int userid { get; set; }
+        public string username{ get; set; }
+        public string password{ get; set; }
+    }
+
+    public class Suppliers
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Get Data From SqlServer/Models/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace JobApp.Models
+{
+    public class Repository
+    {
+        //public static Model Login(Model model)
+        //{
+        //    using (SqlConnection conn = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=App;Trusted_Connection=True;"))
+        //    {
+        //        var query = "SELECT * FROM Users WHERE username = @username AND password = @password";
+        //        var result = conn.QueryFirstOrDefault<Model>(query, new
+        //        {
+        //            username = model.username,
+        //            password = model.password,
+        //        }, commandType: CommandType.Text);
+        //        return result;
+        //    }
+        //}
+        public static async Task<List<Suppliers>> GetSupplier()
+        {
+            using (SqlConnection conn = new SqlConnection("Server=localhost\\SQLEXPRESS;Database=Test;Trusted_Connection=True;"))
+
+            {
+
+                await conn.OpenAsync();
+                var res = await conn.QueryAsync<Suppliers>("select * from Supplier");
+                return res.ToList();
+            }
+        }
+    }
+}
